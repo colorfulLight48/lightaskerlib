191a7c837c5ef5fe2f9fc74ad8ee2075384518db
colorfulLight48 on Mac <188122220+colorfulLight48@users.noreply.github.com>
2026-03-27 15:30:51 -0400

    Tag

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Lightasker-Data",
+    name: "lightaskerlib",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
