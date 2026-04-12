e4e24de20e46deaa65b64a67988c6ca5d4bde465
colorfulLight48 on Mac <188122220+colorfulLight48@users.noreply.github.com>
2026-04-12 12:26:14 -0400

    Delete due date and start date and move them to lightaskerdates (which will be published soon after release)

--- Sources/lightaskerlib/lightaskerlib.swift
+++ Sources/lightaskerlib/lightaskerlib.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 extension Date {
     /// Strips milliseconds
@@ -33,59 +34,7 @@ public struct Task {
     public var id: String?
     public var name: String?    
     public var isCompleted: Bool = false
-    public var dueDate: Date?
-    public var startDate: Date?
-    public var isOverdue: Bool {
-        // 1. If there's no due date, it can't be overdue
-        guard let dueDate = dueDate else { return false }
-        
-        // 2. Compare the due date with the current moment
-        return dueDate < Date()
-    }
-    public var startSecond: Date? {
-        guard let nstartDate = startDate else { return nil } // e.g., 12:00:05.7
-        return nstartDate.toSeconds
-    }
-    public var startMinute: Date? {
-        guard let nstartDate = startDate else { return nil } // e.g., 12:00:05.7
-        return nstartDate.toMinutes
-    }
-    public var startHour: Date? {
-        guard let nstartDate = startDate else { return nil } // e.g., 12:00:05.7
-        return nstartDate.toHours
-    }
-    public var startDay: Date? {
-        guard let nstartDate = startDate else { return nil } // e.g., 12:00:05.7
-        return nstartDate.toDays
-    }
-    public var isStartSecond: Bool {
-        guard let nstartDate = startDate else { return false } // e.g., 12:00:05.7
-        let truncatedStartDate = nstartDate.toSeconds
-        let now = Date() // e.g., 12:00:05.7
-        let truncatedDate = Date(timeIntervalSinceReferenceDate: floor(now.timeIntervalSinceReferenceDate))
-        return truncatedDate == truncatedStartDate
-    }
-    public var isStartMinute: Bool {
-        let now: Date = Date() // 12:05:45
-        let flooredToMinute: Date = now.toMinutes
-        
-        
-        return flooredToMinute == startMinute
-
-    }
-    public var isStartHour: Bool {return Date().toHours == startHour}
-    public var isStartDay: Bool {return Date().toHours == startDay}
-    public var isInPeriod: Bool {
-        if let unwrapped_startDate: Date = startDate, let unwrapped_dueDate: Date = dueDate {
-            return Date() > unwrapped_startDate && Date() < unwrapped_dueDate
-        } else if let unwrapped_startDate: Date = startDate {
-            return Date() > unwrapped_startDate
-        } else if let unwrapped_dueDate: Date = dueDate {
-            return Date() < unwrapped_dueDate
-        } else {
-            return true
-        }
-    }
+   
     public mutating func markComplete() {
         self.isCompleted = true
     }
